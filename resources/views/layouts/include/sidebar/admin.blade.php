3871c2fabdd67a61386f3e6d5687298591a79652
Aqil Farros <aqilghonim@gmail.com>
2024-05-09 13:47:54 +0700

    update

--- resources/views/layouts/include/sidebar/admin.blade.php
+++ resources/views/layouts/include/sidebar/admin.blade.php
@@ -7,7 +7,24 @@
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
-        </li><!-- End Dashboard Nav -->
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link {{ request()->routeIs('admin.user.*' || 'admin.product.*') ? '' : 'collapsed' }}"
+                data-bs-target="#user-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-people"></i><span>User</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="user-nav"
+                class="nav-content collapse {{ request()->routeIs('admin.user.*' || 'admin.product.*') ? 'show' : '' }}"
+                data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="{{ route('admin.user.index') }}"
+                        class="{{ request()->routeIs('admin.user.index') ? 'active' : '' }}">
+                        <i class="bi bi-circle"></i><span>User</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
 
         <li class="nav-item">
             <a class="nav-link {{ request()->routeIs('admin.category.*' || 'admin.product.*') ? '' : 'collapsed' }}"
@@ -29,7 +46,7 @@
                     </a>
                 </li>
             </ul>
-        </li><!-- End Components Nav -->
+        </li>
 
     </ul>
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\ProductGalleryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MyTransactionController;
+use App\Http\Controllers\Admin\ProductGalleryController;
+use App\Http\Controllers\Admin\TransactionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,10 +19,18 @@ Route::name('admin.')->prefix('admin')->middleware('admin')->group(function () {
     Route::resource('/category', CategoryController::class)->except(['show', 'create', 'edit']);
     Route::resource('/product', ProductController::class);
     Route::resource('/product.gallery', ProductGalleryController::class)->except(['create', 'show', 'edit', 'update']);
+    Route::resource('/transaction', TransactionController::class);
+    Route::resource('/my-transaction', MyTransactionController::class);
+    Route::name('user.')->prefix('user')->group(function() {
+        Route::get('/index', [\App\Http\Controllers\Admin\UserController::class, 'index'])->name('index');
+        Route::put('reset-password/{id}', [\App\Http\Controllers\Admin\UserController::class, 'resetPassword'])->name('reset-password');
+    });
 });
 
 Route::name('user.')->prefix('user')->middleware('user')->group(function () {
     Route::get('/dashboard', [\App\Http\Controllers\User\DashboardController::class, 'index'])->name('dashboard');
+    Route::put('/change-password', [\App\Http\Controllers\User\DashboardController::class, 'changePassword'])->name('change-password');
+    Route::resource('/my-transaction', MyTransactionController::class)->only('index', 'show');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
